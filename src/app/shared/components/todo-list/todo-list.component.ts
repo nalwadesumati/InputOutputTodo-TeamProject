55b9bbaaca7bbf75f2dc63ad74dacf6f1a7344b3
mrpawar221@gmail.com <nalwadesumati@gmail.com>
2026-01-18 09:32:55 +0530

    Fix the console error

--- src/app/shared/components/todo-list/todo-list.component.ts
+++ src/app/shared/components/todo-list/todo-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output, TrackByFunction } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  TrackByFunction,
+} from '@angular/core';
 import { Itodo } from '../../model/todo.model';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
@@ -11,35 +18,33 @@ import { findIndex } from 'rxjs';
 })
 export class TodoListComponent implements OnInit {
   @Input() todosArr: Array<Itodo> = [];
-  @Output() emitRemoveId : EventEmitter<string> = new EventEmitter<string>();
+  @Output() emitRemoveId: EventEmitter<string> = new EventEmitter<string>();
+  @Output() emitEditTodo: EventEmitter<any> = new EventEmitter<any>();
+
   // trackById!: TrackByFunction<any>;
 
-  constructor(
-    private _matDialog: MatDialog
-  ) {}
+  constructor(private _matDialog: MatDialog) {}
 
   ngOnInit(): void {}
 
-  onRemove(todo: Itodo){
-    let matConfig =new MatDialogConfig();
-    matConfig.width = "500px";
+  onRemove(todo: Itodo) {
+    let matConfig = new MatDialogConfig();
+    matConfig.width = '500px';
     matConfig.disableClose = true;
     matConfig.data = `Are you sure to delete the todoItem with Id ${todo.todoId}`;
-    let matDialogRef = this._matDialog.open(ConfirmDialogComponent,matConfig)
-    matDialogRef.afterClosed().subscribe(flag =>{
-      if(flag){
+    let matDialogRef = this._matDialog.open(ConfirmDialogComponent, matConfig);
+    matDialogRef.afterClosed().subscribe((flag) => {
+      if (flag) {
         this.emitRemoveId.emit(todo.todoId);
-      //  let getIndex = this.todosArr.findIndex(t=> {
-      //   return t.todoId === todo.todoId
-      //  });
-      //  this.todosArr.splice(getIndex,1);
+        //  let getIndex = this.todosArr.findIndex(t=> {
+        //   return t.todoId === todo.todoId
+        //  });
+        //  this.todosArr.splice(getIndex,1);
       }
-    })
+    });
   }
-onToEdit(todo: Itodo) {
+  onEdit(todo: Itodo) {
     console.log(todo);
     this.emitEditTodo.emit(todo);
   }
-
-  
 }
